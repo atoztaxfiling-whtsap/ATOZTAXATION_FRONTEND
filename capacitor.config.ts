1016343ef5194d5811ae1a09f59e2b3d8cd3a5ef
atoztaxfiling-whtsap <atoztaxfiling@gmail.com>
2026-08-06 06:17:19 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.atoz.taxation',
+  appName: 'ATOZ Taxation',
+  webDir: 'dist'
+};
+
+export default config;
