9b38d85549fae5325b8f3de7e34df4ca70345800
atoztaxfiling-whtsap <atoztaxfiling@gmail.com>
2026-08-16 17:58:45 +0530

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "atoz-v8";
+const CACHE_NAME = "atoz-v9";
 const PRECACHE = ["/", "/index.html", "/icon-192.png"];
 
 self.addEventListener("install", e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(PRECACHE))); self.skipWaiting(); });
